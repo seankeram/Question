355cb2b05bd37aff7e81430c1052dd1bae20523b
shayan keram <shayankeram@gmail.com>
2024-02-23 15:22:26 +0330

    add The result of the question

--- src/component/QuesList.jsx
+++ src/component/QuesList.jsx
@@ -1,13 +1,17 @@
 import { Box, Button, Typography } from "@mui/material";
 const QuesList = (props) => {
-  const { onClick, count } = props;
+  const { onClick, count, result, wrong } = props;
+  console.log(result);
 
   const answerList = [
     {
       id: 1,
       ques: "How do you declare a variable in JavaScript?",
       title: [
-        { isCorrect: false, text: "null is an intentional absence of value" },
+        {
+          isCorrect: false,
+          text: "null is an intentional absence of value",
+        },
         {
           isCorrect: false,
           text: "You can declare a variable using the var, let, or const keywords.",
@@ -53,63 +57,121 @@ const QuesList = (props) => {
     },
   ];
 
-  return (
-    <>
-      <Box>
-        {answerList
-          ?.filter((item) => item?.id === count)
-          .map((answer) => {
-            return (
-              <div size="small" key={answer.id}>
-                <Typography variant="h4" color="initial" padding={2}>
-                  Question
-                </Typography>
-                <Typography
-                  style={{
-                    width: "100%",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    color: "lightgray",
-                    boxShadow: 3,
-                    height: "64px",
-                    backgroundColor: "black",
-                    borderRadius: 5,
-                    padding: 8,
-                  }}
-                >
-                  <Typography fontSize={17} noWrap key={answer}>
-                    {answer.ques}
+  if (count === 4) {
+    return (
+      <>
+        <Typography sx={{ fontWeight: "bold" }}>
+          The result of the question
+        </Typography>
+        <Typography sx={{ width: "100%" }}>
+          <Box
+            sx={{
+              width: "100%",
+              gap: 10,
+              height: 50,
+              bgcolor: "success.main",
+              borderRadius: 5,
+              textTransform: "none",
+              alignItems: "center",
+              fontSize: "13px",
+              marginTop: "30px",
+              fontWeight: "bold",
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <Typography>Correct</Typography>
+            {result}
+          </Box>
+          <Box
+            sx={{
+              width: "100%",
+              gap: 10,
+              height: 50,
+              bgcolor: "red",
+              borderRadius: 5,
+              textTransform: "none",
+              alignItems: "center",
+              fontSize: "13px",
+              marginTop: "30px",
+              fontWeight: "bold",
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <Typography>wrong</Typography>
+            {wrong}
+          </Box>
+        </Typography>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <Box>
+          {answerList
+            ?.filter((item) => item?.id === count)
+            .map((question) => {
+              return (
+                <div size="small" key={question.id}>
+                  <Typography variant="h4" color="initial" padding={2}>
+                    Question
                   </Typography>
-                </Typography>
-
-                {answer.title.map((e) => {
-                  return (
-                    <Button
-                      key={e}
-                      onClick={onClick}
-                      size="small"
-                      variant="contained"
-                      disableElevati
-                      style={{
-                        textTransform: "none",
-                        fontSize: "17px",
-                        marginTop: "30px",
-                        height: "64px",
-                        width: "100%",
-                        display: "flex",
-                        justifyContent: "center",
-                      }}
+                  <Typography
+                    sx={{
+                      width: "100%",
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      color: "lightgray",
+                      boxShadow: 3,
+                      height: "64px",
+                      backgroundColor: "black",
+                      borderRadius: 5,
+                    }}
+                  >
+                    <Typography
+                      fontSize={13}
+                      noWrap
+                      key={question}
+                      fontWeight={"bold"}
                     >
-                      {e.text}
-                    </Button>
-                  );
-                })}
-              </div>
-            );
-          })}
-      </Box>
-    </>
-  );
+                      {question.ques}
+                    </Typography>
+                  </Typography>
+
+                  {question.title.map((answer) => {
+                    return (
+                      <>
+                        <Button
+                          key={answer.text}
+                          onClick={() => onClick(question.id, answer)}
+                          size="small"
+                          variant="contained"
+                          disableElevation
+                          sx={{
+                            borderRadius: 5,
+                            textTransform: "none",
+                            fontSize: "13px",
+                            marginTop: "30px",
+                            fontWeight: "bold",
+                            height: "64px",
+                            width: "100%",
+                            display: "flex",
+                            justifyContent: "center",
+                          }}
+                        >
+                          {answer.text}
+                        </Button>
+                      </>
+                    );
+                  })}
+                </div>
+              );
+            })}
+        </Box>
+      </>
+    );
+  }
 };
 export default QuesList;
--- src/component/questionBox.jsx
+++ src/component/questionBox.jsx
@@ -1,22 +1,44 @@
+import { Box } from "@mui/material";
 import { useState } from "react";
 import QuesList from "./QuesList";
 
 const QuestionBox = () => {
   const [count, setCount] = useState(1);
+  const [data, setData] = useState();
+  const [result, setResult] = useState(0);
+  const [wrong, setWrong] = useState(0);
 
-  const handleClick = () => {
+  const correctList = () => {
+    if (data?.answer?.isCorrect) {
+      setResult(result + 1);
+    } else {
+      setWrong(wrong + 1);
+    }
+  };
+  const handleClick = (id, answer) => {
+    setData({
+      id,
+      answer,
+    });
+
     setCount(count + 1);
+    correctList();
   };
 
   return (
-    <div className="h-[820px]">
-      <div className=" flex items-center justify-center p-4 h-full">
-        <div className="border-spacing-1 border-solid bg-slate-50  rounded-lg shadow-sm p-4 h-full w-full">
+    <Box sx={{ height: "100vh" }}>
+      <Box className=" flex items-center justify-center p-4 h-full">
+        <Box className="border-spacing-1 border-solid bg-slate-50  rounded-lg shadow-sm p-4 h-full w-full">
           {/* <MotionBar></MotionBar> */}
-          <QuesList onClick={handleClick} count={count}></QuesList>
-        </div>
-      </div>
-    </div>
+          <QuesList
+            result={result}
+            wrong={wrong}
+            onClick={handleClick}
+            count={count}
+          ></QuesList>
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
